724aaa2deb31ff162c225210e23a2b8955285fc6
sohamdeb99 <118478834+sohamdeb99@users.noreply.github.com>
2024-05-01 20:17:27 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  entry: './project/src/index.js', // Adjust if your entry point is different
+  entry: './project/src/index.js',  // Entry point of your application
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',          // Output bundle file name
+    path: path.resolve(__dirname, 'dist'),  // Output path
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']  // Loaders for processing CSS files
+      },
+      {
+        test: /\.js$/,                      // Rule for JavaScript files
+        exclude: /node_modules/,            // Exclude the node_modules directory
         use: {
-          loader: 'babel-loader',
+          loader: 'babel-loader',           // Use babel-loader for JavaScript files
           options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+            presets: ['@babel/preset-env'], // Use @babel/preset-env for compiling JS
+          }
+        }
       },
       // Add other loaders for different file types as needed
-    ],
-  },
+    ]
+  }
 };
